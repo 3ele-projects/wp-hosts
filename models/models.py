3fd40c2e023cb39b0ea8afab51a2ce867b13c84d
3ele-projects <github@3ele.de>
2020-08-14 13:59:54 +0200

    add method

--- models/models.py
+++ models/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import logging
+_logger = logging.getLogger(__name__)
 class wp_instance_hosts(models.Model):
     _name = 'wp_instance.wp_hosts'
 
@@ -14,6 +15,9 @@ class wp_instance_hosts(models.Model):
     wp_instances = fields.One2many('wp_instance.wp_core', 'wp_hosts')
     partner = fields.Many2one('res.partner')
 
+       
+
+
 
 class wp_instance_core_inherit(models.Model):
     _inherit = 'wp_instance.wp_core'
@@ -25,3 +29,18 @@ class wp_instance_core_inherit(models.Model):
     ssh_port = fields.Integer(string='Port')
     wp_cli_path = fields.Char(string='wp_cli_path')
     wp_hosts = fields.Many2one('wp_instance.wp_hosts', 'wp_instances')
+    
+    @api.multi
+    def backup_data(self):
+        data_set = []
+        for record in self:
+            data = {}
+            data['wp_host']= record['wp_hosts']['host']
+            data['user']= record['wp_hosts']['user']
+            data['wp_cli_path']= record['wp_hosts']['wp_cli_path']
+            data['port']= record['wp_hosts']['ssh_port']
+            data['wp_path']= record['wp_path']
+            data['sql_path']= record['sql_path']
+            data_set.append(data)
+        _logger.info(data_set)     
+        return data_set
